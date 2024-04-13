ae9ce66a08596ca41575064a93aa48f669db6c1c
RamonBarros <89661742+RamonBarros@users.noreply.github.com>
2024-04-13 11:44:45 -0300

    Ajuste no arquivo "init.sql" para criar corretamente a base de dados "stefanini_database" e a tabela "task" no momento da inicialização do docker do postgresql.

--- backend/apitodolist/database-docker/init.sql
+++ backend/apitodolist/database-docker/init.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE IF NOT EXISTS stefanini_database;
+CREATE DATABASE stefanini_database;
 
 \c stefanini_database;
 
-CREATE TABLE IF NOT EXISTS tasks (
+CREATE TABLE task (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
